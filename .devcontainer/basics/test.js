ac77b6b5f7c909f4066cd70b1734c3649f7a622c
Terol-Chakma <chakmaterol1@gmail.com>
2025-05-03 12:43:26 +0000

    basics with variables

--- .devcontainer/basics/test.js
+++ .devcontainer/basics/test.js
@@ -1,1 +1,17 @@
-console.log("TEROL")
+console.log("TEROL")    // syntax to print
+
+
+// variables
+const acountId = 12345  //cont can't change
+let accountMail = "abc@gmail.com"   //changeable variable
+var accoutPassword = "abc123"   //var is not refferable
+accountCity = kolkata   //possible
+let accountState
+
+/*
+not to use var bcz of issues in block scop
+*/
+
+
+//console.log(acountId)
+console.table([accountId, accountMail,accoutPassword, accountCity, accountState])
